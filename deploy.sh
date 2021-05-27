9ed2bf53ca1421ce98cfe2355c0ce23c3024f40c
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2021-05-27 19:41:14 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:alenapliusnina/alenapliusnina.git master:gh-pages
+git push -f git@github.com:alenapliusnina/SimonTheGame-Vue.js.git master:gh-pages
 
 cd -
